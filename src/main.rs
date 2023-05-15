2227580134200835b67412b0126c6d23b0e5155c
czar <czar.yobero@gmail.com>
2023-05-14 20:16:34 -0700

    add src/main.rs to .gitignore

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let mut blues = 0;
     let mut greens = 0;
 
-    let sample = choice(&colors, 1000, false, Some(&p));
+    let sample = choice(&colors, 1000, true, Some(&p));
     for color in sample {
         match color {
             "red" => reds += 1,
@@ -28,4 +28,7 @@ fn main() {
     let sample = choice(&range, 15, true, Some(&[0.05, 0.05, 0.8, 0.05, 0.05]));
 
     println!("sample: {:?}", sample);
+
+    let data = binomial(5, 0.65, 82);
+    println!("data: {:?}", data);
 }
