dacd78650932baab4e60ea900b0b81b2055a0ed2
czar <czar.yobero@gmail.com>
2023-05-15 11:19:03 -0700

    remove src/main.rs from git repo

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use numrust::*;
-
-fn main() {
-    let x = vec![1., 2., 3., 4., 5.];
-    let y = vec![1., 3., 2.0, 5.0, 4.0];
-    println!("cov of x and y is {:?}", covariance(&x, &y));
-    println!("cov of x and y is {:?}", corrcoef(&x, &y));
-}
